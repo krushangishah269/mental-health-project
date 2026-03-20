afc5900e5222e1aa802f4a608a0071ecde66189d
krushangishah269 <krushangishah269@gmail.com>
2026-03-20 22:41:49 +0530

    final clean setup

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import numpy as np
-
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -31,5 +31,7 @@ def predict():
     return jsonify({"prediction": int(prediction[0])})
 
 if __name__ == "__main__":
-    
+
     app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
